ba0712558011e8e58f9fcd42cd6f0db5b6803b50
danielLeiteSilva <daniel199257@gmail.com>
2023-04-18 13:22:55 -0300

    Altered GPT

--- server.js
+++ server.js
@@ -331,7 +331,7 @@ app.listen(port, async () => {
     
     Com base nessas informações acima, devolva somente um codigo JSON formatado sem quebra de linha, E NADA MAIS, com os seguintes parametros: 
     nota: 0 - 10 
-    description: descricao do porque devo ou não comprar o Apê
+    description: descricao detalhada do porque devo ou não comprar o Apê
     compro: recomendo ou não recomendo
     sentiment: bom, mau, razoável, péssimo, excelente`
     }
