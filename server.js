e1f684bee793323bfe5a140510542b30831e7242
danielLeiteSilva <daniel199257@gmail.com>
2023-04-22 09:11:05 -0300

    Altered server.js

--- server.js
+++ server.js
@@ -527,7 +527,7 @@ app.listen(port, async () => {
 
         return new Promise((resolve) => {
             try {
-                request.post(`http://localhost:8081/pdf`, headers, (error, response, body) => {
+                request.post(`https://api-get-score.herokuapp.com//pdf`, headers, (error, response, body) => {
                     if (!error) {
                         if (response.statusCode === 200) {
                             const response = JSON.parse(body)["file"]
@@ -559,7 +559,7 @@ app.listen(port, async () => {
 
         return new Promise((resolve) => {
             try {
-                request.post(`http://localhost:8081/score`, headers, (error, response, body) => {
+                request.post(`https://api-get-score.herokuapp.com/score`, headers, (error, response, body) => {
                     if (!error) {
                         if (response.statusCode === 200) {
                             const response = JSON.parse(body)["score"]
