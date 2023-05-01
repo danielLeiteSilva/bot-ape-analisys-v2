93839fd480062e85f4acc00ccaf7ea7373482d7a
danielLeiteSilva <daniel199257@gmail.com>
2023-04-30 22:52:36 -0300

    Alteres lat long

--- server.js
+++ server.js
@@ -617,6 +617,7 @@ app.listen(port, async () => {
                 request.get(encodeURI(`https://maps.googleapis.com/maps/api/directions/json?origin=${address}&destination=${addressWork}&mode=transit&transit_mode=bus&key=AIzaSyBUdnRFDvnIE2TKUMH9xIU1ti40mG4jJl0`), (error, response, body) => {
                     if (!error) {
                         if (response.statusCode === 200) {
+                            
                             const distance = JSON.parse(body)["routes"][0]["legs"][0]["distance"]["text"]
                             const duration = JSON.parse(body)["routes"][0]["legs"][0]["duration"]["text"]
                             resolve({ message: { distance, duration }, code: response.statusCode })
