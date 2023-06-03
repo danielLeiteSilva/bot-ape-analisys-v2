03f171cef8c6509d234bdbf1bcd72933b360f4d4
danielLeiteSilva <daniel199257@gmail.com>
2023-06-03 02:32:58 -0300

    Change helth

--- routes.js
+++ routes.js
@@ -2,7 +2,7 @@ const app = require('express').Router()
 const Apartament = require('./src/controller/Apartament')
 const Health = require('./src/controller/Health')
 
-app.get("/api/v1", Health.ok)
+app.get("/", Health.ok)
 app.post('/api/v1/apartaments/register', Apartament.registerApartaments)
 app.get("/api/v1/apartaments/all", Apartament.allApartaments)
 
